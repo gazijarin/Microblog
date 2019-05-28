eff03d484359063e018f4cf3b9fd20491b4266f9
gazijarin <gazijarin@gmail.com>
2019-05-28 10:21:02 -0400

    Made the posts generic

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -9,17 +9,20 @@ def index():
     user = {'username': 'Jarin'}
     posts = [
         {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'author': {'username': 'Sample user 1'},
+            'body': 'Sample post 1'
         },
         {
-            'author': {'username': 'Susan'},
-            'body': 'The Jane Eyre movie was so cool!'
+            'author': {'username': 'Sample user 2'},
+            'body': 'Sample post 2'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Log in', form=form)
