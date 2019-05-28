50c8c70e604bf2c24930eb854ecb8e1b7a5b9ee8
Gazi Jarin <32211479+gazijarin@users.noreply.github.com>
2019-05-28 16:19:48 -0400

    Load user feature from the database given id

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
 from app import db
+from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -25,3 +27,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
