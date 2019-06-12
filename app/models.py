5bd4cd3a2756a13bdbf085d2595984508ad747f7
Gazi Jarin <32211479+gazijarin@users.noreply.github.com>
2019-06-12 11:36:30 -0400

    Added the about me and last seen feature

--- app/models.py
+++ app/models.py
@@ -11,6 +11,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -39,4 +41,5 @@ class Post(db.Model):
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
